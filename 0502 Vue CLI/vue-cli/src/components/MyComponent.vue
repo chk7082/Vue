1b67ded3ad0111e4d802f6bad51ffbd0dbde8661
chk7082 <whgusrl7082@gmail.com>
2023-05-02 11:24:37 +0900

    0502 Vue CLI

---
+++ 0502 Vue CLI/vue-cli/src/components/MyComponent.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="border">
+    <h1>This is MyComponent</h1>
+    <MyChild />
+    <MyChild />
+    <MyChild />
+    <MyChild />
+  </div>
+</template>
+
+<script>
+// import MyChild from './MyChild.vue'
+import MyChild from '@/components/MyChild.vue'
+export default {
+  name: 'MyComponentaaaaa',
+  components: {
+    MyChild,
+  },
+}
+</script>
+
+<style>
+.border {
+  border: solid;
+}
+</style>
