f4ec987c23064a1dbb7ed72ff586a5d374abdc50
chk7082 <whgusrl7082@gmail.com>
2023-05-15 14:52:21 +0900

    0515 Vue Vue with Auth

---
+++ 0515 Vue Vue with Auth/홈워크_vue_10_hw02_P/홈워크_vue_10_hw02_P.vue
@@ -0,0 +1,18 @@
+// App.vue
+
+export default {
+  name: 'App',
+  data: function () {
+    return {
+      isLogin: false,
+    }
+  },
+  created: function () {
+		// 이곳에 들어갈 코드를 작성하시오.
+    if (JSON.parse(localStorage.getItem('jwt'))) {
+      this.isLogin = true
+    } else {
+      this.isLogin = false
+    }
+  },
+}
